fb8a6562187e63316ae39d811d24df1ea062da65
Smogg21 <spazzaglia98@gmail.com>
2024-06-25 12:34:34 -0500

    Added material paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import {SafeAreaView} from 'react-native';
 import {CounterScreen} from './src/presentation/screens/CounterScreen';
+import { PaperProvider } from 'react-native-paper';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      {/* <HelloWorldScreen name="Salvatore Pazzaglia" /> */}
-
-      <CounterScreen />
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={{flex: 1}}>
+        {/* <HelloWorldScreen name="Salvatore Pazzaglia" /> */}
+        <CounterScreen />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,18 +1,26 @@
 import {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {PrimaryButton} from '../components';
+import {Button} from 'react-native-paper';
 
 export const CounterScreen = () => {
   const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{count}</Text>
-
+      {/* 
       <PrimaryButton
         label="Incrementar"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
-      />
+      /> */}
+
+      <Button
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+        mode="contained">
+        Incrementar
+      </Button>
     </View>
   );
 };
