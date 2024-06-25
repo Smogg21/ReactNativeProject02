66f311ccbe1cb8488ff222f85fedcf1992944cc9
Smogg21 <spazzaglia98@gmail.com>
2024-06-25 12:00:06 -0500

    Finished working on button component

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,22 +1,18 @@
 import {useState} from 'react';
-import {Platform, Pressable, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {PrimaryButton} from '../components';
 
 export const CounterScreen = () => {
   const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{count}</Text>
-      <Pressable
+
+      <PrimaryButton
+        label="Incrementar"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
-        style={ ({ pressed }) => [
-          styles.button,
-          pressed && styles.buttonPressed
-        ]}>
-        <Text style={{
-          color: Platform.OS === 'android' ? 'white' : '#4746AB'
-          }}>Incrementar</Text>
-      </Pressable>
+      />
     </View>
   );
 };
@@ -32,12 +28,4 @@ const styles = StyleSheet.create({
     color: 'black',
     fontWeight: '300',
   },
-  button: {
-    backgroundColor: Platform.OS === 'android' ? '#5856D6': 'white',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 10,
-  },buttonPressed: {
-    backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
-  }
 });
