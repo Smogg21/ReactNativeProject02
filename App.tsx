cc752e3037985094815e9e7865cf7f34dff688e6
Smogg21 <spazzaglia98@gmail.com>
2024-06-25 12:46:11 -0500

    Added globalStyles

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import {SafeAreaView} from 'react-native';
 import {CounterScreen} from './src/presentation/screens/CounterScreen';
 import { PaperProvider } from 'react-native-paper';
+import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen';
+
 
 export const App = () => {
   return (
     <PaperProvider>
       <SafeAreaView style={{flex: 1}}>
         {/* <HelloWorldScreen name="Salvatore Pazzaglia" /> */}
-        <CounterScreen />
+        {/* <CounterScreen /> */}
+        <CounterM3Screen/>
       </SafeAreaView>
     </PaperProvider>
   );
