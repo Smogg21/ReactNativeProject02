946caed5ac62a05aa4a7fe60da490d5471dabc11
Smogg21 <spazzaglia98@gmail.com>
2024-06-26 11:26:07 -0500

    Added Icons

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,6 +2,10 @@ import {useState} from 'react';
 import {Text, View} from 'react-native';
 import {globalStyles} from '../theme/global.styles';
 import {FAB} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+
 
 export const CounterM3Screen = () => {
   const [count, setCount] = useState(0);
@@ -9,11 +13,14 @@ export const CounterM3Screen = () => {
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{count}</Text>
 
+
+      {/* <Icon name="accessibility-outline" size={35}/> */}
+
       <FAB
-        label="+1"
         onPress={() => setCount(count + 1)}
         onLongPress={() => setCount(0)}
         style={globalStyles.fab}
+        icon="add-outline"
       />
     </View>
   );
